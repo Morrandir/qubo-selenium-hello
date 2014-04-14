bd19b91164b145a7c40f2b395ed6ab9005fba69d
Qubo Song <sqb_007@hotmail.com>
2014-04-14 10:11:33 +0800

    Updating project structure. Now we can use App as an entry point. Each test case is an extension to the BasicCases class.

--- src/main/java/com/qubo/learning/main/app.java
+++ src/main/java/com/qubo/learning/main/app.java
@@ -1,43 +1,35 @@
 package com.qubo.learning.main;
 
-import com.thoughtworks.selenium.DefaultSelenium;
+import com.qubo.learning.cases.BasicCases;
+import com.qubo.learning.cases.Hello;
+import com.qubo.learning.cases.TC1;
 import org.openqa.selenium.server.SeleniumServer;
+
 
 /**
  * Created by Qubo_Song on 3/26/2014.
  */
-public class app {
+public class App {
 
     public static void main(String[] args) {
 
         SeleniumServer seleniumServer;
-        DefaultSelenium seleniumClient;
-        boolean passed = false;
+        BasicCases hello = new Hello();
 
-        String host = "localhost";
-        int port = 4444;
-        String url = "http://www.baidu.com/";
-        String browserType = "*firefox";
+        boolean passed = false;
 
         try {
             if((seleniumServer = new SeleniumServer()) != null) {
                 seleniumServer.start();
             }
+            passed = hello.test();
+            seleniumServer.stop();
 
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        String keyWordsLocator = "document.getElementById('kw1')";
-        String search = "document.getElementById('su1')";
-        seleniumClient = new DefaultSelenium(host,port,browserType,url);
-        seleniumClient.start();
-        seleniumClient.open(url);
-        seleniumClient.type(keyWordsLocator,"java selenium");
-        seleniumClient.click(search);
-        seleniumClient.waitForPageToLoad("50000");
-        passed = seleniumClient.isTextPresent("java selenium");
-        seleniumClient.stop();
+
 
     }
 }
